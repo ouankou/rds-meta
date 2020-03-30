4bc6f38753df7d3b91d349b4f12eb7d48bfa9de4
Anjia Wang <anjiawang@gmail.com>
2020-03-30 00:05:53 -0400

    Update voting function

--- MetaService.py
+++ MetaService.py
@@ -41,21 +41,35 @@ def majorityVote(obj):
 	for i in range(len(finalAry)):
 		js[i] = finalAry[i]
 
-	#r = json.dumps(js)
-	r = js
-	return(r)
+	return(js)
 
 def weightVote(obj):
 
 	finalAry = []
 
-	Tsanweight = 0.25
+	Tsanweight = 0.881
 
-	Archerweight= 0.25
+	Archerweight= 0.755
 
-	Inspectorweight = 0.25
+	Inspectorweight = 0.723
 
-	Rompweight = 0.25
+	Rompweight = 0.911
+
+	Archer = 0
+
+	Inspector = 0
+
+	Tsan = 0
+
+	Romp = 0
+
+	ArcherVote = 0
+
+	InsepctorVote = 0
+
+	TsanVote = 0
+
+	RompVote = 0
 
 	jsAry = []
 	for i in range(len(obj)):
@@ -72,9 +86,10 @@ def weightVote(obj):
 						js[i] = 'no race'
 			jsAry.append(js)
 	WeightFlag = 0
-
+	x = []
 	for item in jsAry:
 		for key in item.keys():
+			x.append(key)
 			if key == 0:
 				if item[key] != 'no race':
 					ArcherVote = 1
@@ -96,9 +111,27 @@ def weightVote(obj):
 				else:
 					RompVote = 0
 
-	InsepctorVote = 0
+	for item in x:
+		if item == 0:
+			Archer = 1
+		if item == 1:
+			Inspector = 1
+		if item == 2:
+			Tsan = 1
+		if item == 3:
+			Romp = 1
 
-	WeightFlag = Tsanweight * TsanVote + Archerweight * ArcherVote + Inspectorweight * InsepctorVote + Rompweight * RompVote
+	Tsanweight = Tsanweight * Tsan
+
+	Archerweight = Archerweight * Archer
+
+	Inspectorweight = Inspectorweight * Inspector
+
+	Rompweight = Rompweight * Romp
+
+	Sumweight = Tsanweight + Archerweight + Inspectorweight + Rompweight
+
+	WeightFlag = (Tsanweight/Sumweight) * TsanVote + (Archerweight/Sumweight) * ArcherVote + (Inspectorweight/Sumweight) * InsepctorVote + (Rompweight/Sumweight) * RompVote
 
 	if WeightFlag >= 0.5:
 		print("RDS detected a data race by weight vote!")
@@ -116,9 +149,7 @@ def weightVote(obj):
 	for i in range(len(finalAry)):
 		js[i] = finalAry[i]
 
-	#r = json.dumps(js)
-	r = js
-	return(r)
+	return(js)
 
 def randomVote(obj):
 	finalAry = []
@@ -155,7 +186,5 @@ def randomVote(obj):
 	for i in range(len(finalAry)):
 		js[i] = finalAry[i]
 
-	#r = json.dumps(js)
-	r = js
-	return(r)
+	return(js)
 
